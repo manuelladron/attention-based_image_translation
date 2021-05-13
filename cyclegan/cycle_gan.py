6ba83fad468e49317453d244ba6367d238750522
George Cazenavette <gjc006@latech.edu>
2021-05-13 04:13:33 -0400

    added blocks to log dir

--- cyclegan/cycle_gan.py
+++ cyclegan/cycle_gan.py
@@ -564,7 +564,7 @@ if __name__ == '__main__':
     if opts.use_cycle_consistency_loss:
         opts.sample_dir += '_cycle'
 
-    opts.sample_dir += "_patch_{}".format(opts.patch)
+    opts.sample_dir += "_patch_{}_blocks_{}".format(opts.patch, opts.blocks)
 
     opts.patch_dim = (opts.image_size // opts.patch) ** 2
 
